7ae3cf5a35add8e66068d04b42e4c2b14d3b7a70
Bhageswar5 <77909654+Bhageswar5@users.noreply.github.com>
2024-02-06 09:47:20 +0530

    backend with flask

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'D:/poc-multiStep-form-app/dataSQL'
+    UPLOAD_FOLDER = 'D:\Company Project\Q3\poc-multiStep-form-app\dataSQL'
+    
--- venv1/Scripts/activate.bat
+++ venv1/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\poc-multiStep-form-app\backendSQL\venv1
+set VIRTUAL_ENV=D:\Company Project\Q3\poc-multiStep-form-app\backendSQL\venv1
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(venv1) 
 
 :END
 if defined _OLD_CODEPAGE (
